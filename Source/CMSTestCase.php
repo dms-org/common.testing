fe4bc2ddb0322fbafd9ce226a193554721757f31
Elliot Levin <elliotlevin@hotmail.com>
2015-07-07 13:50:37 +1000

    Implement assertThrows method on base test case

--- Source/CMSTestCase.php
+++ Source/CMSTestCase.php
@@ -4,5 +4,20 @@ namespace Iddigital\Cms\Common\Testing;
 
 class CmsTestCase extends \PHPUnit_Framework_TestCase
 {
-    
+    /**
+     * @param callable $operation
+     * @param string   $exception
+     *
+     * @return \Exception
+     */
+    public function assertThrows(callable $operation, $exception = \Exception::class, $message = 'Failed asserting that the operation throws an exception')
+    {
+        try {
+            $operation();
+            $this->fail($message);
+        } catch (\Exception $e) {
+            $this->assertInstanceOf($exception, $e);
+            return $e;
+        }
+    }
 }
