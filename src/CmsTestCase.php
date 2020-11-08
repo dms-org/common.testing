1b289d16c8ccf99d7217fc797a308cf122008444
Elliot Levin <elliotlevin@hotmail.com>
2020-11-08 19:42:49 +1100

    Add back assertArraySubset function to base test class

--- src/CmsTestCase.php
+++ src/CmsTestCase.php
@@ -3,8 +3,10 @@
 namespace Dms\Common\Testing;
 
 use PHPUnit\Framework\TestCase;
+use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;
 
 abstract class CmsTestCase extends TestCase
 {
     use DmsAsserts;
+    use ArraySubsetAsserts;
 }
